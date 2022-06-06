f2b1428fed21f720756ed2cc05ea16b1ad6811f0
Jordan <yarden11111@gmail.com>
2022-06-06 11:29:13 +0300

    fix routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,4 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  get 'operations', to: 'operations#new'
   root "operations#new"
+  post "/operations", to: "operations#create"
 end
